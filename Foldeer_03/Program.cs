cbc999123fee55583626b19057c3efe875b75cb0
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-09-10 15:58:56 +0300

    Task_0031

---
+++ Foldeer_03/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Найти сумму элементов от M до N, N и M заданы
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+ 
+ int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+Console.Write(Sum (m,n));
