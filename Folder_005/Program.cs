8839fce5148bb3f23a0c55b6643ebac90ba62fca
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-09-10 16:04:35 +0300

    Task_005

---
+++ Folder_005/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Написать программу возведения числа А в целую степень B
+
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int DegreeOfNumber(int numberA, int numberB)
+{
+    if (numberB < 1) return 1;
+    return numberA * (DegreeOfNumber(numberA, numberB - 1));
+}
+
+Console.WriteLine($" Число {numberA} в {numberB} степени = {DegreeOfNumber(numberA, numberB)}");
