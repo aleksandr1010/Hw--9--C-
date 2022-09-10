7a8cd0861235549765b0cf9b4b469cd3cc284d3b
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-09-10 16:01:31 +0300

    Task_004

---
+++ Folder_004/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Найти сумму цифр числа
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+int SumDigits(int number)
+{
+    if (number < 1) return 0;
+    int result = 0;
+    return SumDigits(number / 10) + number % 10;
+}
+Console.WriteLine($"Сумма числа: {SumDigits(number)}");
