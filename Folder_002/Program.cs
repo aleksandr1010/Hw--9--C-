f4e9dba01061fe383c312e070b94107ea9b86f4b
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-09-10 15:56:00 +0300

    Task_002

---
+++ Folder_002/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(1, 50);
